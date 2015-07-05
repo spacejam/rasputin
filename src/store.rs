cc2f615e980822baa21a53ddf9b4f6d7337c011d
Tyler Neely <t@jujit.su>
2015-07-04 19:36:59 -0700

    Apply the parmesan.

--- src/store.rs
+++ src/store.rs
@@ -3,32 +3,27 @@ use std::collections::BTreeMap;
 
 pub struct Store<'a> {
   max_offset: u64,
-  data: &'a BTreeMap<String, Box<BTreeMap<u64, &'a [u8]>>>
+  data: Box<BTreeMap<&'a str, Box<BTreeMap<u64, &'a [u8]>>>>
 }
 
 impl<'a> Store<'a> {
   pub fn new<'b>() -> Store<'b> {
     Store {
       max_offset: 0,
-      data: &'b BTreeMap::new()
+      data: Box::new(BTreeMap::new())
     }
   }
 
-  pub fn add(&'a mut self, key: String, value: &'a [u8]) {
+  pub fn add(&'a mut self, key: &'a str, value: &'a [u8]) {
     self.max_offset += 1;
-    match self.data.get_mut(&key) {
-      Some(version_tree) => {
-        if version_tree.insert(self.max_offset, value).is_none() {
-          panic!("Invariant violation; duplicate offset detected");
+    if self.data.get_mut(&key).is_none() {
+        if self.data.insert(key, Box::new(BTreeMap::new())).is_none() {
+            panic!("Invariant violation; duplicate offset detected");
         }
-      },
-      None => {
-        let mut new_version_tree = Box::new(BTreeMap::new());
-        new_version_tree.insert(self.max_offset, value);
-        if self.data.insert(key, new_version_tree).is_none() {
-          panic!("Invariant violation; duplicate offset detected");
-        }
-      }
+    }
+
+    if self.data.get_mut(&key).unwrap().insert(self.max_offset, value).is_none() {
+      panic!("Invariant violation; duplicate offset detected");
     }
   }
 }
