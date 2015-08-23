a11880dbd30ed830fa343cb5c781e6aa9f55c296
Tyler Neely <t@jujit.su>
2015-08-23 16:40:59 -0700

    combine listener and server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,5 @@
 pub mod store;
 pub mod paxos;
 pub mod client;
-pub mod listener;
 pub mod server;
 pub use server::server::Server;
-pub use server::listener::Listener;
--- src/server/server.rs
+++ src/server/server.rs
@@ -1,24 +1,51 @@
 extern crate log;
 use std::io::Error;
+use std::thread;
 
-use server::Listener;
+use mio::*;
+use mio::tcp::{TcpListener, TcpStream};
+
+// Setup some tokens to allow us to identify which event is
+// for which socket.
+const SERVER: Token = Token(0);
 
 pub struct Server {
+    server: TcpListener,
     peers: Vec<String>,   
-    listener: Listener,
 }
 
 impl Server {
     pub fn new(port: u16, peers: Vec<String>) -> Result<Server, Error> {
-        let listener = try!(Listener::new(port, peers.clone()));
+        let addr = format!("0.0.0.0:{}", port).parse().unwrap();
+        let server = try!(TcpListener::bind(&addr));
         Ok(Server {
             peers: peers,
-            listener: listener,
+            server: server,
         })
     }
 
     pub fn start(&mut self) -> Result<(), String> {
-        self.listener.start();
+        let mut event_loop = EventLoop::new().unwrap();
+        event_loop.register(&self.server, SERVER).unwrap();
+        event_loop.run(self).unwrap();
         Err("Not Implemented".to_string())
+    }
+}
+
+impl Handler for Server {
+    type Timeout = ();
+    type Message = ();
+
+    fn ready(&mut self, event_loop: &mut EventLoop<Server>, token: Token, _: EventSet) {
+        match token {
+            SERVER => {
+                println!("in server callback");
+                // Accept and drop the socket immediately, this will close
+                // the socket and notify the client of the EOF.
+                let _ = self.server.accept();
+                println!("after accept");
+            }
+            _ => panic!("unexpected token"),
+        }
     }
 }
