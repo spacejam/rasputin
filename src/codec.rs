68a4eca019dd13e0e3c4650bf25d1dae98283e62
Tyler Neely <t@jujit.su>
2015-08-25 23:18:59 -0700

    add more message framing, and flaky tests

--- src/codec.rs
+++ src/codec.rs
@@ -1,42 +1,76 @@
+use std::io;
 use std::mem;
 
-use bytes::{MutByteBuf, ByteBuf, Buf};
+use bytes::{MutByteBuf, ByteBuf, Buf, MutBuf};
+use mio::{TryWrite, TryRead};
 
 pub trait Codec {
     type Item;
-    fn decode<'a>(&mut self, buf: &mut Buf) -> Option<&'a mut Self::Item>;
+    fn new() -> Self;
+    fn decode(&mut self, buf: &mut ByteBuf) -> Option<Self::Item>;
     fn encode(a: Self::Item) -> ByteBuf;
 }
 
 pub struct Framed {
-    size: usize,
     msg: Option<MutByteBuf>,
-}
-
-impl Framed {
-    pub fn new() -> Framed {
-        Framed {
-            size: 0,
-            msg: None,
-        }
-    }
+    size: usize,
 }
 
 impl Codec for Framed {
     type Item = ByteBuf;
-    fn decode<'a>(&mut self, buf: &mut Buf) -> Option<&'a mut Self::Item> {
-        if self.msg.is_none() {
-            if buf.bytes().len() >= 4 {
-                let mut sz = [0u8;4];
-                assert!(buf.read_slice(&mut sz) == 4);
-                self.size = array_to_usize(sz);
-            }
+
+    fn new() -> Framed {
+        Framed {
+            msg: None,
+            size: 0,
         }
-        None
+    }
+
+    fn decode(&mut self, buf: &mut ByteBuf) -> Option<Self::Item> {
+        // we haven't received enough bytes for the size, don't consume any
+        if self.msg.is_none() && buf.bytes().len() < 4 {
+            return None;
+        }
+
+        // read the size if we need to
+        if self.msg.is_none() {
+            let mut sz = [0u8;4];
+            assert!(buf.read_slice(&mut sz) == 4);
+            let size = array_to_usize(sz);
+            println!("message size: {}", size);
+            self.size = size;
+            self.msg = Some(ByteBuf::mut_with_capacity(size));
+        }
+
+        let mut msg = self.msg.take().unwrap();
+        println!("buf: {:?}", buf.bytes());
+        println!("msg: {:?}", msg.bytes());
+        println!("msg remaining: {}", msg.remaining());
+        // read actual message
+        buf.try_read_buf(&mut msg);
+        println!("buf: {:?}", buf.bytes());
+        println!("msg: {:?}", msg.bytes());
+        println!("msg remaining: {}", msg.remaining());
+        // if we're done, return our Item
+        match msg.bytes().len() == self.size {
+            true => {
+                println!("4");
+                Some(msg.flip())
+            },
+            false => {
+                println!("5");
+                self.msg = Some(msg);
+                None
+            },
+        }
     }
 
     fn encode(item: Self::Item) -> ByteBuf {
-        ByteBuf::from_slice(&[])
+        let b = item.bytes();
+        let mut res = ByteBuf::mut_with_capacity(4 + b.len());
+        res.write_slice(&usize_to_array(b.len()));
+        res.write_slice(b);
+        res.flip()
     }
 }
 
@@ -54,17 +88,41 @@ pub fn array_to_usize(ip: [u8;4]) -> usize {
 #[cfg(test)]
 mod tests {
     extern crate quickcheck;
+    extern crate rand;
+    use self::rand::thread_rng;
+    use self::rand::Rng;
 
     use codec;
-    fn prop(u: usize) -> bool {
+    use codec::Codec;
+    use bytes::{MutByteBuf, ByteBuf, Buf};
+
+    fn array_prop(u: usize) -> bool {
         codec::array_to_usize(codec::usize_to_array(u)) == u
     }
-    
+
     #[test]
     fn test_usize_to_array_to_usize() {
-        quickcheck::quickcheck(prop as fn(usize)->bool);
+        quickcheck::quickcheck(array_prop as fn(usize)->bool);
         let ip = [250,1,2,3];
         assert!(codec::usize_to_array(codec::array_to_usize(ip)) == ip);
     }
-}
 
+    fn framed_prop(sz: usize) -> bool {
+        let mut rng = thread_rng();
+        let mut v: Vec<u8> = rng.gen_iter::<u8>().take(sz).collect();
+        println!("vec: {:?}", v);
+        let mut c = codec::Framed::new();
+        let bytes = ByteBuf::from_slice(&*v);
+        println!("bytes: {:?}", bytes.bytes());
+        let mut encoded = codec::Framed::encode(bytes);
+        println!("encoded: {:?}", encoded.bytes());
+        let decoded = c.decode(&mut encoded).unwrap();
+        println!("decoded: {:?}", decoded.bytes());
+        decoded.bytes() == &*v
+    }
+
+    #[test]
+    fn test_framed_codec() {
+        quickcheck::quickcheck(framed_prop as fn(usize)->bool);
+    }
+}
